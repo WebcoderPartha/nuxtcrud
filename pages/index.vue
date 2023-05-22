ca4383eff3429b9df68acb9209879074b9623201
Parthadeb Mondal <parthadev76@gmail.com>
2023-05-23 00:52:50 +0600

    Home Page list and form design

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,74 @@
 <template>
     <div>
-        <h2 class="text-center">Home</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Data List</h2>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <td>Name</td>
+                                    <td>Email</td>
+                                    <td>Phone</td>
+                                    <td>Address</td>
+                                    <td>Image</td>
+                                    <td>Action</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Name</td>
+                                    <td>Email</td>
+                                    <td>Phone</td>
+                                    <td>Address</td>
+                                    <td>Image</td>
+                                    <td>Action</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Add Data</h2>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="">Name</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Email</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Phone</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Address</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Image</label>
+                                <input type="file" class="form-control">
+                            </div>
+                            <div class="form-group mt-4">
+                            
+                                <input type="submit" class="btn btn-primary" value="Store">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
