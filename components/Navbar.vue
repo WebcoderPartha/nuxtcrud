72f9eba50affb83c8e013011876975e393e25c26
Parthadeb Mondal <parthadev76@gmail.com>
2023-05-23 00:45:11 +0600

    Nuxt Home Page

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+   
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <Navbar />
+        <slot/>
+    </div>
+</template>
+
+<script setup>
+    useHead({
+        link: [
+            {
+                href: '/assets/css/bootstrap.min.css',
+                rel: 'stylesheet'
+            }
+        ],
+        script: [
+            {
+                src: '/assets/js/bootstrap.min.js',
+                tagPosition: 'bodyClose'
+            },
+            {
+                src: '/assets/js/bootstrap.bundle.min.js',
+                tagPosition: 'bodyClose'
+            
+            }
+        ]
+    })
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
